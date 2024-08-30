dbb4429e76053c97641bca19da042363da2899b3
鍛冶典彦 <kaji@jprs.co.jp>
2024-08-30 16:45:59 +0900

    DestroyServlet.javaを作成

--- src/controllers/DestroyServlet.java
+++ src/controllers/DestroyServlet.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -14,16 +13,16 @@ import models.Message;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class CreateServlet
+ * Servlet implementation class DestroyServlet
  */
-@WebServlet("/create")
-public class CreateServlet extends HttpServlet {
+@WebServlet("/destroy")
+public class DestroyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public CreateServlet() {
+    public DestroyServlet() {
         super();
     }
 
@@ -31,26 +30,18 @@ public class CreateServlet extends HttpServlet {
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String  _token = request.getParameter("_token");
+        String _token = request.getParameter("_token");
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
+
+            Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
+
             em.getTransaction().begin();
-
-            Message m= new Message();
-
-            String title = request.getParameter("title");
-            m.setTitle(title);
-
-            String content = request.getParameter("content");
-            m.setContent(content);
-
-            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            m.setCreated_at(currentTime);
-            m.setUpdated_at(currentTime);
-
-            em.persist(m);
+            em.remove(m);
             em.getTransaction().commit();
             em.close();
+
+            request.getSession().removeAttribute("message_id");
 
             response.sendRedirect(request.getContextPath() + "/Index");
         }
